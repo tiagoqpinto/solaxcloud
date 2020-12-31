21e19cb94ecbe83bcc9e17798fa1b6506a42849c
David Bucher <d.bucher97@gmail.com>
2020-12-31 13:58:21 +0100

    Works with SolaxCloud API now

--- README.md
+++ README.md
@@ -1,14 +1,34 @@
 # SolaxCloud integration for Home Assistant.
 
+Solax Clound integration based on the (now publicly accssible) SolaxCloud API.
+This component is basically a rewrite of [thomascys/solaxcloud](https://github.com/thomascys/solaxcloud).
+You need:
+
+- The API Key which is accessible in the Service menu of your SolaxCloud.
+- The Inverter Serial Number (SN) is the 10 character Registration No. of the
+  specific inverter (see the Inverters menu in SolaxCloud)
+
 ## Installation
-- Place this solaxcloud directory under the custom_components directory.
+
+- Place this directory in `/config/cusotm_components`. If `cusotm_components`
+  does not exist, you have to create it.
 - Add the sensor to your configuration.yaml:
-    ```
-    sensor:
-    - platform: solaxcloud
-      username: USERNAME
-      password: PASSWORD
-      site_id: SITE ID
-    ```
-- To obtain your site id. Login to solaxcloud.com and click on Sites in the left menu. Inspect the Site-name with your browser developer tools. You should see something like "showAnalysis('xxxxxxxxxxxxxx','Home')" in the html code.
-- Verify that the custom entities are available in home assistant.
+  ```
+  sensor:
+  - platform: solaxcloud
+    name: Inverter 1
+    api_key: YOUR_API_KEY
+    sn: YOUR_INVERTER_SN
+  ```
+- Verify that the custom entities are available in home assistant (Total Yield,
+  Daily Yield and AC Power).
+
+## Multiple Inverters
+
+You might have multiple inverters in your PV installation. By simply adding the
+same configuration again, but changing the inverter SN, you can add more
+inverters to home assistant.
+
+```
+
+```
--- sensor.py
+++ sensor.py
@@ -5,7 +5,7 @@ import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
 
 from datetime import timedelta
-from datetime import date
+from datetime import datetime
 
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
@@ -13,74 +13,56 @@ from homeassistant.components.sensor import PLATFORM_SCHEMA
 
 MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=5)
 
-CONF_USERNAME = "username"
-CONF_PASSWORD = "password"
-CONF_SITE_ID = "site_id"
+CONF_NAME = "name"
+CONF_API_KEY = "api_key"
+CONF_SN = "sn"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
-        vol.Required(CONF_USERNAME): cv.string,
-        vol.Required(CONF_PASSWORD): cv.string,
-        vol.Required(CONF_SITE_ID): cv.string,
+        vol.Required(CONF_NAME): cv.string,
+        vol.Required(CONF_API_KEY): cv.string,
+        vol.Required(CONF_SN): cv.string
     }
 )
 
+
 def setup_platform(hass, config, add_entities, discovery_info=None):
-    solax_cloud = SolaxCloud(hass, config[CONF_USERNAME], config[CONF_PASSWORD], config[CONF_SITE_ID])
-    add_entities([SolarPowerSensor(hass, solax_cloud),
-                  GridPowerSensor(hass, solax_cloud),
-                  InverterTemperatureSensor(hass, solax_cloud),
-                  InverterTotalYieldSensor(hass, solax_cloud),
-                  InverterDailyYieldSensor(hass, solax_cloud)
-                 ], True)
+    solax_cloud = SolaxCloud(
+        hass, config[CONF_NAME], config[CONF_API_KEY], config[CONF_SN])
+    add_entities([InverterTotalYieldSensor(hass, solax_cloud),
+                  InverterDailyYieldSensor(hass, solax_cloud),
+                  InverterACPowerSensor(hass, solax_cloud)
+                  ], True)
+
 
 class SolaxCloud:
-    def __init__(self, hass, username, password, site_id):
+    def __init__(self, hass, name, api_key, sn):
         self.hass = hass
         self.logger = logging.getLogger(__name__)
-        self._username = username
-        self._password = password
-        self._site_id = site_id
-        self._base_url = 'http://www.solaxcloud.com:6080'
-        self._login_endpoint = '/proxy/login/login'
-        self._data_endpoint = '/proxy/mysite/getInverterInfo'
-        self.data = []
-        self.inverter_name = 'Solax'
+        self.api_key = api_key
+        self.sn = sn
+        self.inverter_name = name
+        self.data = {}
+        # maybe the user has to change .eu
+        self.uri = f'https://www.eu.solaxcloud.com:9443/proxy/api/getRealtimeInfo.do?tokenId={api_key}&sn={sn}'
+        self.last_data_time = None
 
-    def get_token_id(self):
-        resp = None
-        try:
-            uri = self._base_url + self._login_endpoint + '?userName=' + self._username + '&password=' + self._password
-            resp = requests.post(uri)
-            json_obj = resp.json()
-
-            # {'exception': '用户名或密码输入错误!', 'result': None, 'success': False}
-            if json_obj['success'] == False:
-                if json_obj['exception'] == '用户名或密码输入错误!':
-                    self.logger.error('Invalid Username or Password')
-                elif json_obj['exception'] != '用户名或密码输入错误!':
-                    self.logger.error('Other error:\r\n%s', json_obj['exception'])
-
-                return json_obj
-            else:
-                return json_obj['result']['tokenId']
-        except requests.exceptions.ConnectionError:
-            self.logger.error('Connection Error:\r\n%s', resp)
-            raise ConnectionAbortedError
-
-
-    # http://www.solaxcloud.com:6080/proxy/mysite/getInverterInfo?siteId=<SiteId>&tokenId=<TokenId>
     def get_data(self):
-        resp = None
-        try:
-            token_id = self.get_token_id()
-            uri = self._base_url + self._data_endpoint + '?siteId=' + self._site_id + '&tokenId=' + token_id
-            resp = requests.post(uri)
-            json_obj = resp.json()
-            self.data = json_obj['result'].pop()
-            self.inverter_name = self.data['inverterSN']
-        except requests.exceptions.ConnectionError:
-            return resp
+        if not self.data or datetime.now() - self.last_data_time > MIN_TIME_BETWEEN_UPDATES:
+            try:
+                data = requests.get(self.uri).json()
+                if data['success'] == True:
+                    self.data = data['result']
+                    self.last_data_time = datetime.now()
+                    self.logger.info(
+                        f'Retrieved new data from solax cloud {self.inverter_name}')
+                else:
+                    self.data = {}
+                    self.logger.error(data['exception'])
+            except requests.exceptions.ConnectionError as e:
+                self.logger.error(str(e))
+                self.data = {}
+
 
 class InverterTotalYieldSensor(Entity):
     def __init__(self, hass, solax_cloud):
@@ -94,12 +76,12 @@ class InverterTotalYieldSensor(Entity):
 
     @property
     def state(self):
-        data = self.solax_cloud.data
-        return int(data['totalYield'])
+        data = self.solax_cloud.data.get('yieldtotal')
+        return float('nan') if data is None else data
 
     @property
     def unit_of_measurement(self):
-        return 'kW'
+        return 'kWh'
 
     @property
     def icon(self):
@@ -108,6 +90,7 @@ class InverterTotalYieldSensor(Entity):
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
     def update(self):
         self.solax_cloud.get_data()
+
 
 class InverterDailyYieldSensor(Entity):
     def __init__(self, hass, solax_cloud):
@@ -121,8 +104,36 @@ class InverterDailyYieldSensor(Entity):
 
     @property
     def state(self):
-        data = self.solax_cloud.data
-        return int(data['todayYield'])
+        data = self.solax_cloud.data.get('yieldtoday')
+        return float('nan') if data is None else data
+
+    @property
+    def unit_of_measurement(self):
+        return 'kWh'
+
+    @property
+    def icon(self):
+        return 'mdi:solar-power'
+
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    def update(self):
+        self.solax_cloud.get_data()
+
+
+class InverterACPowerSensor(Entity):
+    def __init__(self, hass, solax_cloud):
+        self._name = solax_cloud.inverter_name + ' AC Power'
+        self.hass = hass
+        self.solax_cloud = solax_cloud
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def state(self):
+        data = self.solax_cloud.data.get('acpower')
+        return float('nan') if data is None else data
 
     @property
     def unit_of_measurement(self):
@@ -135,143 +146,3 @@ class InverterDailyYieldSensor(Entity):
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
     def update(self):
         self.solax_cloud.get_data()
-
-class InverterTemperatureSensor(Entity):
-    def __init__(self, hass, solax_cloud):
-        self._name = solax_cloud.inverter_name + ' Temperature'
-        self.hass = hass
-        self.solax_cloud = solax_cloud
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def state(self):
-        data = self.solax_cloud.data
-        return int(data['temperature'])
-
-    @property
-    def unit_of_measurement(self):
-        return 'C'
-
-    @property
-    def icon(self):
-        return 'mdi:thermometer'
-
-    @Throttle(MIN_TIME_BETWEEN_UPDATES)
-    def update(self):
-        self.solax_cloud.get_data()
-
-class SolarPowerSensor(Entity):
-    def __init__(self, hass, solax_cloud):
-        self._state = None
-        self.hass = hass
-        self._name = solax_cloud.inverter_name + ' PV power'
-        self.solax_cloud = solax_cloud
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def state(self):
-        data = self.solax_cloud.data
-        return int(data['powerdc1'] + data['powerdc2'])
-
-    @property
-    def unit_of_measurement(self):
-        return 'W'
-
-    @property
-    def icon(self):
-        return 'mdi:solar-power'
-
-    @Throttle(MIN_TIME_BETWEEN_UPDATES)
-    def update(self):
-        self.solax_cloud.get_data()
-
-class GridPowerSensor(Entity):
-    def __init__(self, hass, solax_cloud):
-        self._state = None
-        self.hass = hass
-        self._name = solax_cloud.inverter_name +' Grid power'
-        self.solax_cloud = solax_cloud
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def state(self):
-        data = self.solax_cloud.data
-        return int(self.solax_cloud.data['gridPower'])
-
-    @property
-    def unit_of_measurement(self):
-        return 'W'
-
-    @property
-    def icon(self):
-        return 'mdi:solar-power'
-
-    @Throttle(MIN_TIME_BETWEEN_UPDATES)
-    def update(self):
-        self.solax_cloud.get_data()
-
-# Other setup with info on enumerating the type fields
-# https://github.com/wills106/homeassistant-config/blob/master/packages/solax.yaml
-
-# Response example:
-# {
-#     "exception": "查询成功!",
-#     "result": [
-#         {
-#             "wifiSN": "SWSMMS9LCT",
-#             "deviceName": null,
-#             "inverterSN": "XBE502F0000000",
-#             "accessTime": 1588579594000,
-#             "lastUpdateTime": 1588683933442,
-#             "pv1Current": 0.0,
-#             "pv2Current": 0.0,
-#             "pv3Current": null,
-#             "pv4Current": null,
-#             "pv1Voltage": 81.8,
-#             "pv2Voltage": 79.1,
-#             "pv3Voltage": null,
-#             "pv4Voltage": null,
-#             "powerdc1": 0.0,
-#             "powerdc2": 0.0,
-#             "powerdc3": null,
-#             "powerdc4": null,
-#             "totalYield": 30.0,
-#             "todayYield": 27.7,
-#             "iac1": 0.2,
-#             "vac1": 228.7,
-#             "pac1": 0.0,
-#             "fac1": 49.97,
-#             "iac2": 0.0,
-#             "vac2": 0.0,
-#             "pac2": 0.0,
-#             "fac2": 0.0,
-#             "iac3": 0.0,
-#             "vac3": 0.0,
-#             "pac3": 0.0,
-#             "fac3": 0.0,
-#             "gridPower": 0.0,
-#             "feedinPower": 0.0,
-#             "temperature": 34.0,
-#             "hours": 28,
-#             "accessTimes": "2020-05-04 16:06:34",
-#             "lastUpdateTimes": "2020-05-05 21:05:33",
-#             "phaseFlag": 1,
-#             "batteryFlag": 0,
-#             "inverterType": "4",
-#             "enableFlag": "1",
-#             "workMode": 0,
-#             "pvCount": 2,
-#             "transportType": 2
-#         }
-#     ],
-#     "success": true
-# }
